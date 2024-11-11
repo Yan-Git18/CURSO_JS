894cb1629395e3c00034f71c47c91e3fc56e9624
Yan-Git18 <yancarlos180203@gmail.com>
2024-11-10 22:07:40 -0500

    S1-Variables

---
+++ Variables/variables.js
@@ -0,0 +1,16 @@
+let nombre = prompt("Cuál es tu nombre ?");
+let edad = parseInt(prompt("Cuál es tu edad ?"));
+let num1 = parseInt(prompt("El número 1 es: "));
+let num2 = parseInt(prompt("El número 2 es: "));
+
+let texto = "Hola mundo";
+let suma = num1 + num2
+let mult = num1 * num2
+
+
+console.log("El nombre es: " + nombre);
+console.log("El texto es: " + texto);
+console.log("La suma es: " + suma);
+console.log("La multiplicación es: " + mult);
+console.log("Tu edad es: " + edad);
+
